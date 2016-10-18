e12f469d357a370a532de96587e90f27717a2a77
gmsantos <gabriel.ms1@hotmail.com>
2016-10-18 16:56:10 -0200

    change password to batata

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,7 +17,7 @@ $factory->define(Meetup\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => $password ?: $password = bcrypt('batata'),
         'remember_token' => str_random(10),
     ];
 });
